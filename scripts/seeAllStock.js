590e1b0d50b93f0445505b6ce5b4243ff255b94c
zyh66615 <42971334+zyh66615@users.noreply.github.com>
2022-01-20 17:21:58 +0800

    update

--- scripts/seeAllStock.js
+++ scripts/seeAllStock.js
@@ -11,7 +11,7 @@ function localeHHMMSS(ms = 0) {
 
 function sellShorts(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const shortSellValue = ns.sellShort(stockSymbol, stockInfo.sharesShort)
+  const shortSellValue = ns.stock.sell(stockSymbol, stockInfo.sharesShort)
 
   if (shortSellValue) {
     ns.print(
@@ -22,10 +22,10 @@ function sellShorts(ns, stockSymbol) {
     )
   }
 }
-
+/** @param {NS} ns **/
 function sellLongs(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const longSellValue = ns.sellShort(stockSymbol, stockInfo.sharesLong)
+  const longSellValue = ns.stock.sell(stockSymbol, stockInfo.sharesLong)
 
   if (longSellValue) {
     ns.print(
@@ -36,12 +36,12 @@ function sellLongs(ns, stockSymbol) {
     )
   }
 }
-
+/** @param {NS} ns **/
 function getStockInfo(ns, stockSymbol) {
-  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.getStockPosition(stockSymbol)
+  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.stock.getPosition(stockSymbol)
 
-  const stockAskPrice = ns.getStockAskPrice(stockSymbol)
-  const stockBidPrice = ns.getStockBidPrice(stockSymbol)
+  const stockAskPrice = ns.stock.getAskPrice(stockSymbol)
+  const stockBidPrice = ns.stock.getBidPrice(stockSymbol)
 
   return {
     stockSymbol,
@@ -57,7 +57,7 @@ function getStockInfo(ns, stockSymbol) {
 /** @param {NS} ns **/export async function main(ns) {
   ns.disableLog('ALL')
 
-  stockSymbols = ns.getStockSymbols()
+  stockSymbols = ns.stock.getSymbols()
   stockSymbols.forEach((stockSymbol) => {
     sellLongs(ns, stockSymbol)
     sellShorts(ns, stockSymbol)
