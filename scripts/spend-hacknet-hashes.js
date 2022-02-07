b4c92cb5d14f2449f2bbb231187a2fa2df34bdd6
zyh66615 <42971334+zyh66615@users.noreply.github.com>
2022-02-07 20:15:43 +0800

    update

---
+++ scripts/spend-hacknet-hashes.js
@@ -0,0 +1,57 @@
+import { disableLogs, formatDuration } from './helpers.js'
+
+const argsSchema = [
+    ['v', false], // Verbose
+    ['verbose', false],
+    ['l', false], // Turn all hashes into money
+    ['liquidate', false],
+    ['interval', 1000], // Rate at which the program runs and spends hashes
+    ['spend-on', 'Sell for Money'],
+    ['spend-on-server', undefined],
+];
+
+const purchaseOptions = ['Sell for Money', 'Sell for Corporation Funds', 'Exchange for Corporation Research', 'Generate Coding Contract', 'Improve Studying', 'Improve Gym Training'];
+
+export function autocomplete(data, args) {
+    data.flags(argsSchema);
+    const lastFlag = args.length > 1 ? args[args.length - 2] : null;
+    if (lastFlag == "--spend-on") // Provide a couple auto-complete options to facilitate these arguments with spaces in them
+        return purchaseOptions.map(f => f.replaceAll(" ", "_")).sort().concat(purchaseOptions.map(f => `'${f}'`).sort());
+    return [];
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    const options = ns.flags(argsSchema);
+    const verbose = options.v || options.verbose;
+    const liquidate = options.l || options.liquidate;
+    const interval = options.interval;
+    const toBuy = options['spend-on'].replaceAll("_", " ");
+    const spendOnServer = options['spend-on-server']?.replaceAll("_", " ") ?? undefined;
+    disableLogs(ns, ['sleep']);
+    ns.print(`Starting spend-hacknet-hashes.js to ensure no hashes go unspent. Will check in every ${formatDuration(interval)}`);
+    ns.print(liquidate ? `-l --liquidate mode active! Will spend all hashes on money as soon as possible.` :
+        `Only spending hashes every when near capacity to avoid wasting them.`);
+    while (true) {
+        let capacity = ns.hacknet.hashCapacity() || 0;
+        let startingHashes = ns.hacknet.numHashes() || 0;
+        let nodes = ns.hacknet.numNodes();
+        if (capacity == 0 && nodes > 0)
+            return ns.print('We have hacknet nodes, not hacknet servers, so spending hashes is not applicable.');
+
+        let globalProduction = Array.from({ length: nodes }, (_, i) => ns.hacknet.getNodeStats(i))
+            .reduce((total, node) => total + node.production, 0);
+        //ns.print(`Current hacknet production: ${globalProduction.toPrecision(3)}...`);
+        // Spend hashes before we lose them
+        let reserve = 10 + globalProduction * interval / 1000; // If we are this far from our capacity, start spending
+        let success = true;
+        while (success && ns.hacknet.numHashes() > (liquidate ? 4 : capacity - reserve))
+            success = ns.hacknet.spendHashes(toBuy, spendOnServer);
+        if (!success)
+            ns.print(`Weird, failed to spend hashes. (Have: ${ns.hacknet.numHashes()} Capacity: ${ns.hacknet.hashCapacity()}`);
+        if (verbose && ns.hacknet.numHashes() < startingHashes)
+            ns.print(`Spent ${(startingHashes - ns.hacknet.numHashes()).toFixed(0)} hashes` +
+                (liquidate ? '' : ` to avoid reaching capacity (${capacity})`) + ` at ${globalProduction.toPrecision(3)} hashes per second`);
+        await ns.sleep(interval);
+    }
+}
---
+++ scripts/stats.js
@@ -0,0 +1,85 @@
+import { formatNumberShort, formatMoney, getNsDataThroughFile, getActiveSourceFiles } from './helpers.js'
+
+const argsSchema = [
+    ['hide-stocks', false],
+];
+
+export function autocomplete(data, args) {
+    data.flags(argsSchema);
+    return [];
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    const options = ns.flags(argsSchema);
+    const doc = eval('document');
+    const hook0 = doc.getElementById('overview-extra-hook-0');
+    const hook1 = doc.getElementById('overview-extra-hook-1');
+    let stkSymbols = null;
+    let dictSourceFiles = await getActiveSourceFiles(ns); // Find out what source files the user has unlocked
+    let playerInfo = (await getNsDataThroughFile(ns, 'ns.getPlayer()', '/Temp/player-info.txt'));
+    if (!options['hide-stocks'] && playerInfo.hasTixApiAccess) // Auto-disabled if we do not have the TSK API
+        stkSymbols = await getNsDataThroughFile(ns, `ns.stock.getSymbols()`, '/Temp/stock-symbols.txt');
+    // Main stats update loop
+    while (true) {
+        try {
+            const headers = []
+            const values = [];
+
+            if (9 in dictSourceFiles) { // Section not relevant if you don't have access to hacknet servers
+                const hashes = await getNsDataThroughFile(ns, '[ns.hacknet.numHashes(), ns.hacknet.hashCapacity()]', '/Temp/hash-stats.txt')
+                if (hashes[1] > 0) {
+                    headers.push("Hashes");
+                    values.push(`${formatNumberShort(hashes[0], 3, 1)}/${formatNumberShort(hashes[1], 3, 1)}`);
+                }
+                // Detect and notify the HUD if we are liquidating
+                if (ns.ps("home").some(p => p.filename.includes('spend-hacknet-hashes') && (p.args.includes("--liquidate") || p.args.includes("-l")))) {
+                    headers.splice(1, 0, " ");
+                    values.push("Liquidating");
+                }
+            }
+
+            if (stkSymbols && !doc.getElementById("stock-display-1")) { // Don't add stocks if unavailable or the stockmaster HUD is active
+                const stkPortfolio = await getNsDataThroughFile(ns, JSON.stringify(stkSymbols) +
+                    `.map(sym => ({ sym, pos: ns.stock.getPosition(sym), ask: ns.stock.getAskPrice(sym), bid: ns.stock.getBidPrice(sym) }))` +
+                    `.reduce((total, stk) => total + stk.pos[0] * stk.bid + stk.pos[2] * (stk.pos[3] * 2 - stk.ask) -100000 * (stk.pos[0] + stk.pos[2] > 0 ? 1 : 0), 0)`,
+                    '/Temp/stock-portfolio-value.txt');
+                if (stkPortfolio > 0) { // Don't bother showing a section for stock if we aren't holding anything
+                    headers.push("Stock");
+                    values.push(formatMoney(stkPortfolio));
+                }
+            }
+            headers.push("ScrInc");
+            values.push(formatMoney(ns.getScriptIncome()[0], 3, 2) + '/sec');
+
+            headers.push("ScrExp");
+            values.push(formatNumberShort(ns.getScriptExpGain(), 3, 2) + '/sec');
+
+            if (2 in dictSourceFiles) { // Gang income is only relevant once gangs are unlocked
+                const gangInfo = await getNsDataThroughFile(ns, 'ns.gang.inGang() ? ns.gang.getGangInformation() : false', '/Temp/gang-stats.txt');
+                if (gangInfo !== false) {
+                    headers.push("Gang");
+                    values.push(formatMoney(gangInfo.moneyGainRate * 5, 3, 2) + '/sec');
+                }
+            }
+
+            const karma = ns.heart.break();
+            if (karma <= -9) {
+                headers.push("Karma");
+                values.push(formatNumberShort(karma, 3, 2));
+            }
+
+            const sharePower = ns.getSharePower();
+            if (sharePower > 1) {
+                headers.push("Share Pwr");
+                values.push(formatNumberShort(sharePower, 3, 2));
+            }
+
+            hook0.innerText = headers.join(" \n");
+            hook1.innerText = values.join("\n");
+        } catch (err) { // Might run out of ram from time to time, since we use it dynamically
+            ns.print("ERROR: Update Skipped: " + String(err));
+        }
+        await ns.sleep(1000);
+    }
+}
